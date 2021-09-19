c92ff3c209696c96e4c477783e5a9d02b36bd2d5
Johon Lieghton <74516939+johonlieghton@users.noreply.github.com>
2021-09-19 17:28:36 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Detenemos y Eliminamos todos los contenedores creados
-#cd /home/devops/Sites/sarapico
 
 #Detenemos todos los contenedores en ejecucion
 docker stop $(docker ps -a -q)
@@ -9,6 +8,6 @@ docker stop $(docker ps -a -q)
 #Eliminamos todos los contenedores existentes
 docker rm $(docker ps -a -q)
 
-#Eliminamos las imagenes debian y sarapico creadas
+#Eliminamos las imagenes "debian" y "sarapico" creadas
 docker rmi debian
 docker rmi sarapico
