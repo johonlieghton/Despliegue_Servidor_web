0c83eb8bf4a38ef9fa5ff805f817e0a47dde01c4
Johon Lieghton <74516939+johonlieghton@users.noreply.github.com>
2021-09-19 13:22:40 -0300

    Update scriptweb.sh

--- scriptweb.sh
+++ scriptweb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+cd /home/devops/Sites/sarapico
 echo "El directorio es : "
 pwd
 echo "Usuario logeado como : "
 whoami
 
-firefox http://localhost:4000
+firefox http://localhost:4000/
 
